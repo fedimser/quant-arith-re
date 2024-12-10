832ce646839c453cbdaea56871a77153f52504d0
Dmytro Fedoriaka <fedimser@gmail.com>
2024-12-09 21:10:19 -0800

    Start implementing PG2012 paper

---
+++ test/PG2012_test.py
@@ -0,0 +1,32 @@
+import pytest
+from qsharp import init, eval
+import random
+import time
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup():
+    init(project_root='.')
+
+
+@pytest.mark.parametrize("n", [2, 3, 5, 8, 10])
+def test_FADD(n: int):
+    for _ in range(5):
+        a, b = random.randint(0, 2**n-1),  random.randint(0, 2**n-1)
+        t0 = time.time()
+        ans = eval(f"QuantumArithmetic.PG2012Test.TestFADD({n},{a},{b})")
+        print(n, time.time()-t0)
+        expected = (a+b) % (2**n)
+        assert ans == expected
+
+
+@pytest.mark.parametrize("n", [1, 2, 3, 4, 5])
+def test_FMAC(n: int):
+    for _ in range(5):
+        a, b, x = [random.randint(0, 2**n-1) for _ in range(3)]
+        print(n, a, b, x)
+        t0 = time.time()
+        ans = eval(f"QuantumArithmetic.PG2012Test.TestFMAC({n},{a},{b},{x})")
+        print(n, time.time()-t0)
+        expected = (b+a*x) % (2**(2*n))
+        assert ans == expected

4687decb768cc85a63b4714e3b314e96011b8ba1
Dmytro Fedoriaka <fedimser@gmail.com>
2024-12-24 10:48:20 -0800

    DKRS: adder modulo 2^n-1(placeholder)

--- test/DKRS2004_test.py
+++ test/DKRS2004_test.py
@@ -19,7 +19,6 @@ def test_Add(n: int):
         assert ans == expected
 
 
-
 @pytest.mark.parametrize("n", [1, 2, 3, 4, 5, 8, 16, 31, 32, 62])
 def test_AddWithCarry(n: int):
     op = "QuantumArithmetic.DKRS2004.AddWithCarry"
@@ -29,3 +28,14 @@ def test_AddWithCarry(n: int):
         ans = eval(f"TestUtils.BinaryOpInPlaceExtraOut({n},{a},{b},{op})")
         expected = a+b
         assert ans == expected
+
+
+@pytest.mark.parametrize("n", [1, 2, 3, 4, 5, 8, 16, 31, 32, 62])
+def test_AddMod2nm1OutOfPlace(n: int):
+    MOD = 2**n-1
+    op = "QuantumArithmetic.DKRS2004.AddMod2nm1OutOfPlace"
+    for _ in range(5):
+        a = random.randint(0, 2**n-1)
+        b = random.randint(0, 2**n-1)
+        ans = eval(f"TestUtils.BinaryOp({n},{a},{b},{op})")
+        assert ans % MOD == (a+b) % MOD

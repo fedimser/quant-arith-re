58b891a25d72657c68399f9e4cb10efca3cc02e5
Dmytro Fedoriaka <fedimser@gmail.com>
2024-12-31 13:20:42 -0800

    LYTZW2013: Implement Incrementer_v2 (unoptimized, fig 7)

--- test/LYTZW2013_test.py
+++ test/LYTZW2013_test.py
@@ -8,10 +8,26 @@ def setup():
     init(project_root='.')
 
 
-@pytest.mark.parametrize("op", ["QuantumArithmetic.LYTZW2013.Increment_v1"])
+@pytest.mark.parametrize("op", [
+    "QuantumArithmetic.LYTZW2013.Increment_v1",
+    "QuantumArithmetic.LYTZW2013.Increment_v2"
+])
 @pytest.mark.parametrize("n", [1, 2, 3, 4, 5])
 def test_Increment_Exhaustive(op: str, n: int):
     N = 2**n
     for x in range(N):
+        ans = eval(f"TestUtils.UnaryOpInPlace({n},{x}L,{op})")
+        assert ans == (x+1) % N
+
+
+@pytest.mark.parametrize("op", [
+    "QuantumArithmetic.LYTZW2013.Increment_v1",
+    "QuantumArithmetic.LYTZW2013.Increment_v2"
+])
+@pytest.mark.parametrize("n", [16, 32])
+def test_Increment(op: str, n: int):
+    N = 2**n
+    for _ in range(5):
+        x = random.randint(0, N-1)
         ans = eval(f"TestUtils.UnaryOpInPlace({n},{x}L,{op})")
         assert ans == (x+1) % N

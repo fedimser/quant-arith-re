6d8edea87840903f1151a30f28ebcd4c9cf4f615
Dmytro Fedoriaka <fedimser@gmail.com>
2026-06-14 14:26:54 -0700

    Implement FlipFirst

--- lib/src/QuantumArithmetic/LAInc.qs
+++ lib/src/QuantumArithmetic/LAInc.qs
@@ -32,4 +32,42 @@ operation CountTrailingOnes(x : Qubit[], ans : Qubit[]) : Unit is Ctl + Adj {
             Controlled CountTrailingOnes([ans[n-1]], (x_high, ans[0..n-2]));
         }
     }
-} 
+}
+
+/// Flips first `ctr` bits in `target`.
+/// If `ctr==0`, does nothing.
+/// If `ctr==Length(target)`, flips all bits.
+/// If `ctr>Length(target)`, behavior is undefined.
+operation FlipFirst(target : Qubit[], ctr : Qubit[]) : Unit is Ctl + Adj {
+    let target_len = Length(target);
+    let ctr_len = Length(ctr);
+    let n = Std.Math.Floor(Log2(target_len)) + 1;
+    if (ctr_len > n) {
+        // Counter too large, ignore highest qubits.
+        FlipFirst(target, ctr[0..n-1]);
+    } elif (ctr_len < n) {
+        // Counter too small, can only affect prefix of target.
+        FlipFirst(target[0..(1 <<< ctr_len)-2], ctr);
+    } elif (target_len == 1) {
+        CNOT(ctr[0], target[0]);
+    } elif (target_len == 2) {
+        CNOT(ctr[0], target[0]);
+        CNOT(ctr[1], target[0]);
+        CNOT(ctr[1], target[1]);
+    } else {
+        Std.Diagnostics.Fact(ctr_len == n, "");
+        Std.Diagnostics.Fact(target_len >= (1 <<< (n - 1)), "");
+        let target_low : Qubit[] = target[0..(1 <<< (n - 1))-1];
+        let target_high : Qubit[] = target[(1 <<< (n - 1))..target_len-1];
+
+        Controlled ApplyToEachCA([ctr[n-1]], (X, target_low));
+        if (Length(target_high) > 0) {
+            Controlled FlipFirst([ctr[n-1]], (target_high, ctr[0..n-2]));
+        }
+        if (Length(target_low) > 1) {
+            X(ctr[n-1]);
+            Controlled FlipFirst([ctr[n-1]], (target_low[0..Length(target_low)-2], ctr[0..n-2]));
+            X(ctr[n-1]);
+        }
+    }
+}
--- test/LAInc_test.py
+++ test/LAInc_test.py
@@ -1,8 +1,9 @@
 import math
+import random
 
 import pytest
 
-from test_utils import  ArithmeticOpTester
+from test_utils import ArithmeticOpTester
 
 
 def _ctz(x: int) -> int:
@@ -15,12 +16,37 @@ def _cto(x: int) -> int:
     return _ctz(x + 1)
 
 
-@pytest.mark.parametrize("x_size", [1, 2, 3, 4, 5, 6, 7, 8])
-def test_CountLowestOnes(x_size: int):
-    """Tests CountLowestOnes."""
+@pytest.mark.parametrize("x_size", [1, 2, 3, 4, 5, 6])
+def test_CountTrailingOnes_exhaustive(x_size: int):
     op = "QuantumArithmetic.LAInc.CountTrailingOnes"
     ans_size = math.floor(math.log2(x_size)) + 1
     tester = ArithmeticOpTester(op, [x_size, ans_size])
     for x in range(2**x_size):
         result = tester.run([x, 0])
         assert result == [x, _cto(x)]
+
+
+@pytest.mark.parametrize("x_size", [10, 20, 30])
+def test_CountTrailingOnes(x_size: int):
+    op = "QuantumArithmetic.LAInc.CountTrailingOnes"
+    ans_size = math.floor(math.log2(x_size)) + 1
+    tester = ArithmeticOpTester(op, [x_size, ans_size])
+    for ones_count in range(0, x_size + 1):
+        x = 2**ones_count - 1
+        r = x_size - (ones_count + 1)
+        if r > 0:
+            x += (random.randint(0, 2**r - 1)) << (ones_count + 1)
+        result = tester.run([x, 0])
+        assert result == [x, ones_count]
+
+
+@pytest.mark.parametrize("target_size", [1, 2, 3, 4, 5, 6, 7, 8, 10, 20, 30])
+def test_FlipFirst(target_size: int):
+    op = "QuantumArithmetic.LAInc.FlipFirst"
+    ctr_size = math.floor(math.log2(target_size)) + 1
+    tester = ArithmeticOpTester(op, [target_size, ctr_size])
+    for flip_count in range(target_size + 1):
+        assert flip_count < 2**ctr_size
+        target_init = random.randint(0, 2**target_size - 1)
+        result = tester.run([target_init, flip_count])
+        assert result == [target_init ^ ((1 << flip_count) - 1), flip_count]

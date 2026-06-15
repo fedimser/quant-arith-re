330dad818a1eefaae38cfe65eac74d4bd821108e
Dmytro Fedoriaka <fedimser@gmail.com>
2026-06-14 20:50:07 -0700

    update tests for DKRS

--- test/DKRS2004_test.py
+++ test/DKRS2004_test.py
@@ -1,30 +1,26 @@
 import pytest
-from qsharp import init, eval
 import random
 
-
-@pytest.fixture(scope="session", autouse=True)
-def setup():
-    init(project_root="./lib/")
+from test_utils import ArithmeticOpTester
 
 
 @pytest.mark.parametrize("n", [1, 2, 3, 4, 5, 8, 16, 31, 32, 63])
 def test_Add(n: int):
     op = "QuantumArithmetic.DKRS2004.Add"
+    tester = ArithmeticOpTester(op, [n, n])
     for _ in range(5):
-        a = random.randint(0, 2**n-1)
-        b = random.randint(0, 2**n-1)
-        ans = eval(f"TestUtils.BinaryOpInPlace({n},{a}L,{b}L,{op})")
-        expected = (a+b) % (2**n)
-        assert ans == expected
+        a = random.randint(0, 2**n - 1)
+        b = random.randint(0, 2**n - 1)
+        ans = tester.run([a, b])
+        assert ans == [a, (a + b) % (2**n)]
 
 
 @pytest.mark.parametrize("n", [1, 2, 3, 4, 5, 8, 16, 31, 32, 62])
 def test_AddWithCarry(n: int):
-    op = "QuantumArithmetic.DKRS2004.AddWithCarry"
+    op = "((a,b,c)=>QuantumArithmetic.DKRS2004.AddWithCarry(a,b,c[0]))"
+    tester = ArithmeticOpTester(op, [n, n, 1])
     for _ in range(5):
         a = random.randint(0, 2**n-1)
         b = random.randint(0, 2**n-1)
-        ans = eval(f"TestUtils.BinaryOpInPlaceExtraOut({n},{a}L,{b}L,{op})")
-        expected = a+b
-        assert ans == expected
+        ans = tester.run([a, b, 0])
+        assert ans == [a, (a + b) % (2**n), (a + b) // (2**n)]
